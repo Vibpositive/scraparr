f833b73d2037172f01c86a1c358df8ca2679ee4d
Gabriel Oliveira <ras.vibpositive@gmail.com>
2025-04-21 22:49:42 +0100

    Adds support to accept untrusted certs

--- src/scraparr/connectors/util.py
+++ src/scraparr/connectors/util.py
@@ -6,12 +6,27 @@ This module contains helper functions to avoid duplicate code.
 
 import logging
 import requests
+import urllib3
 
+from scraparr.parser import ConfigParser
+
+config_parser = ConfigParser()
+config_parser.parse_env_config()
+
+requests_verify = True
+ssl_config = config_parser.config.get('ssl')
+if ssl_config and ssl_config.get('accept_insecure_certificates') is True:
+    requests_verify = False
 
 def get(api_url, api_key):
     """Get data from API and Logs errors"""
     try:
-        r = requests.get(api_url, headers={"X-Api-Key": api_key}, timeout=20)
+        if requests_verify is False:
+            with urllib3.warnings.catch_warnings(): # type: ignore
+                urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+                r = requests.get(api_url, headers={"X-Api-Key": api_key}, verify=True, timeout=20)
+        else:
+            r = requests.get(api_url, headers={"X-Api-Key": api_key}, verify=True, timeout=20)
         if r.status_code == 200:
             return r.json()
         if r.status_code == 401:
--- src/scraparr/parser/__init__.py
+++ src/scraparr/parser/__init__.py
@@ -1,43 +1,65 @@
 import os
-from dotenv import dotenv_values
-from typing import Optional, Dict, Mapping
+from typing import Optional, Dict, Mapping, Union
 
+ConfigDict = Dict[str, Union[str, bool]]
+FullConfig = Dict[str, Optional[ConfigDict]]
 
-def _build_config(env: Mapping[str, str]) -> Dict[str, Optional[Dict[str, str]]]:
+class ConfigParser:
+
     services = [
         'sonarr', 'radarr', 'prowlarr',
         'bazarr', 'readarr', 'jellyseerr', 'overseerr'
     ]
-    config: Dict[str, Optional[Dict[str, str]]] = {
-        'general': None,
+
+    config: FullConfig = {
+        'general': {},
+        'ssl': {
+            'accept_insecure_certificates': False,
+        },
         'auth': None,
     }
 
-    optional_fields = ['alias', 'api_version', 'interval', 'detailed']
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ConfigParser, cls).__new__(cls)
+        return cls.instance
 
-    for service in services:
-        prefix = service.upper()
-        url = env.get(f'{prefix}_URL')
-        api_key = env.get(f'{prefix}_API_KEY')
+    def _build_config(self, env: Mapping[str, str]) -> FullConfig:
+        
 
-        if url and api_key:
-            service_config = {
-                'url': url,
-                'api_key': api_key,
-                **{
-                    field: val
-                    for field in optional_fields
-                    if (val := env.get(f'{prefix}_{field.upper()}')) is not None
+        accept_insecure_certificates = env.get('SCRAPARR_ACCEPT_INSECURE_CERTIFICATES')
+        if 'ssl' not in self.config or self.config['ssl'] is None:
+            self.config['ssl'] = {}
+
+        self.config['ssl']['accept_insecure_certificates'] = (
+            accept_insecure_certificates.lower() == 'true' if accept_insecure_certificates else False
+        )
+
+        optional_fields = ['alias', 'api_version', 'interval', 'detailed']
+
+        for service in self.services:
+            prefix = service.upper()
+            url = env.get(f'{prefix}_URL')
+            api_key = env.get(f'{prefix}_API_KEY')
+
+            if url and api_key:
+                service_config: ConfigDict = {
+                    'url': url,
+                    'api_key': api_key,
+                    **{
+                        field: val
+                        for field in optional_fields
+                        if (val := env.get(f'{prefix}_{field.upper()}')) is not None
+                    }
                 }
-            }
-            config[service] = service_config
-        else:
-            config[service] = None
+                self.config[service] = service_config
+            else:
+                self.config[service] = None
 
-    return config
+        return self.config
 
-def parse_dotenv_config(path: str = "/scraparr/.env") -> Dict[str, Optional[Dict[str, str]]]:
-    return _build_config(dotenv_values(path)) # type: ignore
+    def parse_dotenv_config(self, path: str = "/scraparr/.env") -> FullConfig:
+        return self._build_config(dotenv_values(path))  # type: ignore
 
-def parse_env_config() -> Dict[str, Optional[Dict[str, str]]]:
-    return _build_config(os.environ)
+    def parse_env_config(self) -> FullConfig:
+        return self._build_config(os.environ)
